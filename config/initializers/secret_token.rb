9e25d1d2322ac21b73aeade62ad58dde15ca25ee
Andrew Ngan <ngan7487@gmail.com>
2011-05-25 03:10:32 +0800

    Initial commit, downgraded rake, devise + devise views, username added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proto6::Application.config.secret_token = '449c3e25846185fe1dccf4602430cf85599ef9fc50c756b21485973a75e614df897584e7de45d0073b2ba553317d43753b24dba79f9d010178d51d99f5490f71'
